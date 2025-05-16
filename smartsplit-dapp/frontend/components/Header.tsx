381b30abd7155a0a088e5f2c9f2af2660024c66c
lennartschaeffer <ln546006@dal.ca>
2025-05-16 07:22:02 -0400

    fixed title

--- smartsplit-dapp/frontend/components/Header.tsx
+++ smartsplit-dapp/frontend/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <div className="flex items-center justify-center px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
       <h1 className="display bg-gradient-to-r mr-40 from-purple-400 to-blue-400 text-transparent bg-clip-text
-">SplitShare</h1>
+">SmartSplit</h1>
 
       <div className="flex gap-2 items-center flex-wrap">
         <WalletSelector />
